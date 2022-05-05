dbc1f2b3304adc782ec809dd38ce14f5e9a3ff80
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-05 11:54:00 +0200

    (Js edited, desktop view fixed)

---
+++ js/index.js
@@ -0,0 +1,10 @@
+function signUp(){
+    const fname = document.getElementById('nameUser').value;
+    const surname = document.getElementById('sUser');
+    const email = document.getElementById('email');
+    const password = document.getElementById('phoneNumber');
+    
+    console.log(fname);
+
+}
+signUp()
