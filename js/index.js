fcce00d57276051eee34c29bc9e1b4438f369e89
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-05 16:19:31 +0200

    {forms} updated

--- js/index.js
+++ js/index.js
@@ -1,14 +1,17 @@
 function signUp(){
+    
     const fname = document.getElementById('nameUser').value;
-    const surname = document.getElementById('sUser');
-    const email = document.getElementById('email');
-    const password = document.getElementById('phoneNumber');
+    const surname = document.getElementById('sUser').value;
+    const email = document.getElementById('email').value;
+    const phoneNumber = document.getElementById('phoneNumber').value;
+    const pwd = document.getElementById('pwd').value;
+
     
     console.log(fname);
     console.log(surname);
     console.log(email);
-    console.log(password);
-    
+    console.log(phoneNumber);
+    console.log(pwd);
 
 }
 signUp()
