1fd089ec2dc16589490a83449f99b9193fe3eaba
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-10 11:16:33 +0200

    {Wide scrren added]

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.getElementById('signupButton').onclick = function(){
+let person ={
+    name:'Edmond',
+    surname:"Sadiki",
+    email:'Tendanisadiki16@gmail.com',
+    phoneNumber: '0711741781',
+    password:'user1'
+}
+let username = document.getElementById('name')= person.name;
+console.log(username)
+}
+
