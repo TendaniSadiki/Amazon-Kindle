a5815a1fb748c019bb2e510e4e07d1dd3f03dd8f
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-12 16:24:37 +0200

    {
    page fixed}

--- js/index.js
+++ js/index.js
@@ -7,15 +7,71 @@ function left() {
     alert("pageXOffset: " + window.scrollY + ", scrollY: " + window.scrollX);
   }
 
-document.getElementById('signupButton').onclick = function(){
-let person ={
-    name:'Edmond',
-    surname:"Sadiki",
-    email:'Tendanisadiki16@gmail.com',
-    phoneNumber: '0711741781',
-    password:'user1'
+  var objUsers = [
+    {
+        username: "amahle",
+        email: "amahlemthembu@reader.com",
+        password: "bookworm-7"
+    },
+    {
+        username: "basetsana",
+        email: "bassie997@launcher.com",
+        password: "bassiereads"
+    },
+    {
+        username: "colin",
+        email: "colinjones@tester.com",
+        password: "loveebooks"
+    }
+];
+function getInfo(){
+    var username = document.getElementsByClassName("username")[0].value;
+    var password = document.getElementsByClassName("password")[0].value;
+    for(i = 0; i < objUsers.length; i++) {
+        if(username == objUsers[i].username && password == objUsers[i].password) {
+            window.location.href = "index.html";
+            return;
+       } else if(username == objUsers[i].email && password == objUsers[i].password){
+            window.location.href = "index.html";
+            return;
+       }
+    }
+    alert("incorrect username or password");
 }
-let username = document.getElementById('name')= person.name;
-console.log(username)
+// js  for registration form
+function () {
+    var fname = document.getElementById("nameUser").value;
+    var lname = document.getElementById("surname").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var pass = document.getElementById("pwd").value;
+   // alert("details logged");
+    for(i = 0; i < objUsers.length; i++) {
+        if (fname == objUsers[i].username || email == objUsers[i].email) {
+            alert("Username not available");
+            return;
+        }
+    }
+        if(pass == passconfirm) {
+        objUsers.push(
+            {
+                username: fname,
+                email: email,
+                password: pass
+            },
+        )
+        window.location.href = "index.html";
+    }
+        else {
+        alert("password does not match");
+        return;
+    }
 }
+
+
+
+
+
+
+
 
