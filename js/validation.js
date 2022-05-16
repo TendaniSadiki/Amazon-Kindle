ff42f31e459fb2dc08168b53ee9ede1bbc10cd85
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-16 15:04:08 +0200

    {session storage added}

--- js/validation.js
+++ js/validation.js
@@ -1,4 +1,35 @@
+1
 function signup(){
-let  siteName = "My site";
-localStorage.setItem("setename", siteName)
+var names = document.getElementById('name');
+var surname = document.getElementById('surname');
+var mail = document.getElementById('email');
+var phoneNumber = document.getElementById('phoneNumber');
+var Password = document.getElementById('pwd');
+var imagess = document.getElementById('imagess');
+    sessionStorage.setItem('name', names.value);
+    sessionStorage.setItem('surname',surname.value);
+    sessionStorage.setItem('mail',mail.value);
+    sessionStorage.setItem('phonenumber',phoneNumber.value);
+    sessionStorage.setItem('Password', Password.value);
+    sessionStorage.setItem('imagess', imagess.value);
+    alert('Your account has been created');
+    window.open("index.html");
+}
+//document.getElementById('loginResult').innerHTML=localStorage.getItem('name');
+function signin(){
+    var logmail=sessionStorage.getItem('mail');
+    var lopw=sessionStorage.getItem('Password');
+    var maibox=document.getElementById('email').value;
+    var passbox=document.getElementById('pwd').value;
+    var location = '';
+    if(maibox != logmail ){
+        alert("Wrong Email") ;
+this.location.href = location;
+    }
+    else if(passbox != lopw){
+      alert("Wrong Password");
+    }else{
+        alert("Welcome " +sessionStorage.getItem('name'));
+        window.open("home.html");
+    }
 }
