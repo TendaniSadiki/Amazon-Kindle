8679e9f655d519f7e7808351a6c630c8af88bade
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-17 10:07:04 +0200

    {validation{ addded}

--- validation.js
+++ validation.js
@@ -3,17 +3,50 @@ function signup(){
 var names = document.getElementById('name');
 var surname = document.getElementById('surname');
 var mail = document.getElementById('email');
-var phoneNumber = document.getElementById('phoneNumber');
 var Password = document.getElementById('pwd');
-var imagess = document.getElementById('image');
+var lowerCaseLetters = /[a-z]/g;
+    var upperCaseLetters = /[A-Z]/g;
+    var numbers = /[0-9]/g;
+
+
+if(names.value.length == 0 && surname.value.length == 0 && mail.value.length == 0 && Password.value.length == 0){
+    alert('Please fill in all the spaces');
+
+}
+else if(names.value.length == 0){
+    alert('Please fill in Username');
+
+}else if(surname.value.length == 0){
+    alert('Please fill in Surname');
+
+}else if(mail.value.length == 0){
+    alert('Please fill in Email');
+
+}else if(Password.value.length == 0){
+    alert('Please fill in password');
+
+}else if(Password.value.length > 8){
+    alert('Max of 8');
+
+}else if(!Password.value.match(numbers)){
+    alert('please add 1 number');
+
+}else if(!Password.value.match(upperCaseLetters)){
+    alert('please add 1 uppercase letter');
+
+}else if(!Password.value.match(lowerCaseLetters)){
+    alert('please add 1 lovercase letter');
+
+}
+else{
     sessionStorage.setItem('name', names.value);
     sessionStorage.setItem('surname',surname.value);
     sessionStorage.setItem('mail',mail.value);
-    sessionStorage.setItem('phonenumber',phoneNumber.value);
     sessionStorage.setItem('Password', Password.value);
-    sessionStorage.setItem('imagess', imagess.value);
     alert('Your account has been created');
     window.open("index.html");
+    
+}
 }
 
 function signin(){
@@ -24,12 +57,12 @@ function signin(){
     var location = '';
     if(maibox != logmail ){
         alert("Wrong Email") ;
-this.location.href = location;
+        this.location.href = location;
     }
     else if(passbox != lopw){
       alert("Wrong Password");
     }else{
         alert("Welcome " +sessionStorage.getItem('name'));
-window.open("home.html");
+        window.open("home.html");
     }
 }
