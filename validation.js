957020d6d91982f15ee83d6b1e22eb9307b5db07
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-17 16:44:44 +0200

    {VALIDATION] EDITED

--- validation.js
+++ validation.js
@@ -8,43 +8,49 @@ var lowerCaseLetters = /[a-z]/g;
     var upperCaseLetters = /[A-Z]/g;
     var numbers = /[0-9]/g;
 
-
+    sessionStorage.setItem('name', names.value);
+    sessionStorage.setItem('surname',surname.value);
+    sessionStorage.setItem('mail',mail.value);
+    sessionStorage.setItem('Password', Password.value);
 if(names.value.length == 0 && surname.value.length == 0 && mail.value.length == 0 && Password.value.length == 0){
-    alert('Please fill in all the spaces');
+    alert('Please fill in all inputs');
 
 }
 else if(names.value.length == 0){
     alert('Please fill in Username');
 
-}else if(surname.value.length == 0){
+}
+else if(surname.value.length == 0){
     alert('Please fill in Surname');
 
-}else if(mail.value.length == 0){
+}
+else if(mail.value.length == 0){
     alert('Please fill in Email');
 
-}else if(Password.value.length == 0){
+}
+else if(Password.value.length == 0){
     alert('Please fill in password');
 
-}else if(Password.value.length > 8){
+}
+else if(Password.value.length > 8){
     alert('Max of 8');
 
-}else if(!Password.value.match(numbers)){
+}
+else if(!Password.value.match(numbers)){
     alert('please add 1 number');
 
-}else if(!Password.value.match(upperCaseLetters)){
+}
+else if(!Password.value.match(upperCaseLetters)){
     alert('please add 1 uppercase letter');
 
-}else if(!Password.value.match(lowerCaseLetters)){
+}
+else if(!Password.value.match(lowerCaseLetters)){
     alert('please add 1 lovercase letter');
 
 }
 else{
-    sessionStorage.setItem('name', names.value);
-    sessionStorage.setItem('surname',surname.value);
-    sessionStorage.setItem('mail',mail.value);
-    sessionStorage.setItem('Password', Password.value);
     alert('Your account has been created');
-    window.open("index.html");
+    window.location.href("index.html");
     
 }
 }
@@ -55,14 +61,9 @@ function signin(){
     var maibox=document.getElementById('email').value;
     var passbox=document.getElementById('pwd').value;
     var location = '';
-    if(maibox != logmail ){
-        alert("Wrong Email") ;
-        this.location.href = location;
-    }
-    else if(passbox != lopw){
-      alert("Wrong Password");
+    if(userName.value == storedName && userPw.value == storedPw){
+        alert('You are logged in.');
     }else{
-        alert("Welcome " +sessionStorage.getItem('name'));
-        window.open("home.html");
+        alert('Error on login');
     }
 }
