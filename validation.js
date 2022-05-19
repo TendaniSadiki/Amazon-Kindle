26e6135bb8cf6380fbc17c61d4bf9940bd2b0652
Edmond Sadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-05-19 12:10:48 +0200

    sign in fixed

--- validation.js
+++ validation.js
@@ -5,13 +5,14 @@ var surname = document.getElementById('surname');
 var mail = document.getElementById('email');
 var Password = document.getElementById('pwd');
 var lowerCaseLetters = /[a-z]/g;
-    var upperCaseLetters = /[A-Z]/g;
-    var numbers = /[0-9]/g;
+var upperCaseLetters = /[A-Z]/g;
+var numbers = /[0-9]/g;
 
     sessionStorage.setItem('name', names.value);
     sessionStorage.setItem('surname',surname.value);
     sessionStorage.setItem('mail',mail.value);
     sessionStorage.setItem('Password', Password.value);
+    
 if(names.value.length == 0 && surname.value.length == 0 && mail.value.length == 0 && Password.value.length == 0){
     alert('Please fill in all inputs');
 
@@ -52,8 +53,11 @@ else{
     alert('Your account has been created');
     window.location.href("index.html");
     
+    
 }
+
 }
+
 
 function signin(){
     var logmail=sessionStorage.getItem('mail');
@@ -61,27 +65,50 @@ function signin(){
     var maibox=document.getElementById('email').value;
     var passbox=document.getElementById('pwd').value;
     var location = '';
-    var location = '';
-    if(maibox == '' && passbox == ''){
-        alert('Please fill in all inputs');
-    
-    }
-    else if(maibox != logmail ){
-        alert("Wrong Email") ;
-this.location.href = location;
-    }
-    else if(passbox != lopw){
-      alert("Wrong Password");
-    }
-    else if(maibox.value.length == 0){
-        alert('Please fill in Email');
-    
-    }
-    else if(passbox.value.length == 0){
-        alert('Please fill in password');
-    
-    }
-    else if(maibox == logmail && passbox == lopw ){
-        alert("Welcome " +sessionStorage.getItem('name'));
+
+    if(maibox == logmail &&passbox==lopw){
+        alert("Your Email & Password correct") ;
         window.open("home.html");
-    }}
+    }
+    else if(maibox !=passbox &&passbox != lopw ){
+      alert("Wrong Email or Password");
+    }
+    else if(maibox==''&&passbox==''){
+        alert("Your Email & Password empty");
+    }
+    else{
+        alert("Welcome " +sessionStorage.getItem('userName'));
+window.open("home.html");}
+   
+    // if(maibox == '' && passbox == ''){
+    //     alert('Please fill in all inputs');
+    //     window.open("home.html");
+    // }
+    // else if(maibox != logmail ){
+    //     alert("Wrong Email") ;
+    //     window.open("home.html");
+    // }
+    // else if(passbox != lopw){
+    //   alert("Wrong Password");
+    //   window.open("home.html");
+    // }
+    // else if(maibox.value.length == 0){
+    //     alert('Please fill in Email');
+    //     window.open("home.html");
+    // }
+    // else if(passbox.value.length == 0){
+    //     alert('Please fill in password');
+    //     window.open("home.html");
+    
+    // }
+    // else {
+    //     alert("Welcome " +sessionStorage.getItem('name'));
+    //     window.open("home.html");
+    // }
+       }
+
+    function chaneTheImage(){
+        const img=document.getElementById("userImg");
+        img.src=sessionStorage.getItem("file");
+        document.getElementById("userName").innerText = sessionStorage.getItem("name");
+    }
