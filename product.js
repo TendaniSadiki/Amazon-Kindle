b489d1919e8f8fe69392dbda6ddabe730d2f0039
TendaniSadiki <61437061+TendaniSadiki@users.noreply.github.com>
2022-06-03 02:07:44 +0200

    {cart working}

--- product.js
+++ product.js
@@ -10,7 +10,7 @@ const products = [
       inCart:0,
       productDescription:"22 seconds... until Lindsay Boxer loses her badge—or her life.SFPD Sergeant Lindsay Boxer has guns on her mind.There’s buzz of a last-ditch shipment of drugs and weapons crossing the Mexican border ahead of new restrictive gun laws. Before Lindsay can act, her top informant tips her to a case that hits disturbingly close to home.Former cops. Professional hits. All with the same warning scrawled on their bodies:You talk, you die. Now it’s Lindsay’s turn to choose.",
       isbn:123456678,
-      imgSrc:"./assets/image/book1.jpg",
+      imgSrc:"./assets/bookImg/book1.jpg",
     },
     {
         id: 1,
@@ -19,7 +19,7 @@ const products = [
         inCart:0,
         productDescription:"22 seconds... until Lindsay Boxer loses her badge—or her life.SFPD Sergeant Lindsay Boxer has guns on her mind.There’s buzz of a last-ditch shipment of drugs and weapons crossing the Mexican border ahead of new restrictive gun laws. Before Lindsay can act, her top informant tips her to a case that hits disturbingly close to home.Former cops. Professional hits. All with the same warning scrawled on their bodies:You talk, you die. Now it’s Lindsay’s turn to choose.",
         isbn:123456678,
-        imgSrc:"./assets/image/book2.jpg",
+        imgSrc:"./assets/bookImg/book2.jpg",
       },
       {
         id: 2,
@@ -28,7 +28,7 @@ const products = [
         inCart:0,
         productDescription:"22 seconds... until Lindsay Boxer loses her badge—or her life.SFPD Sergeant Lindsay Boxer has guns on her mind.There’s buzz of a last-ditch shipment of drugs and weapons crossing the Mexican border ahead of new restrictive gun laws. Before Lindsay can act, her top informant tips her to a case that hits disturbingly close to home.Former cops. Professional hits. All with the same warning scrawled on their bodies:You talk, you die. Now it’s Lindsay’s turn to choose.",
         isbn:123456678,
-        imgSrc:"./assets/image/book3.jpg",
+        imgSrc:"./assets/bookImg/book3.jpg",
       },
       {
         id: 3,
@@ -37,7 +37,7 @@ const products = [
         inCart:0,
         productDescription:"22 seconds... until Lindsay Boxer loses her badge—or her life.SFPD Sergeant Lindsay Boxer has guns on her mind.There’s buzz of a last-ditch shipment of drugs and weapons crossing the Mexican border ahead of new restrictive gun laws. Before Lindsay can act, her top informant tips her to a case that hits disturbingly close to home.Former cops. Professional hits. All with the same warning scrawled on their bodies:You talk, you die. Now it’s Lindsay’s turn to choose.",
         isbn:123456678,
-        imgSrc:"./assets/image/book4.jpg",
+        imgSrc:"./assets/bookImg/book4.jpg",
       },
       {
         id: 4,
@@ -46,7 +46,7 @@ const products = [
         inCart:0,
         productDescription:"22 seconds... until Lindsay Boxer loses her badge—or her life.SFPD Sergeant Lindsay Boxer has guns on her mind.There’s buzz of a last-ditch shipment of drugs and weapons crossing the Mexican border ahead of new restrictive gun laws. Before Lindsay can act, her top informant tips her to a case that hits disturbingly close to home.Former cops. Professional hits. All with the same warning scrawled on their bodies:You talk, you die. Now it’s Lindsay’s turn to choose.",
         isbn:123456678,
-        imgSrc:"./assets/image/book5.jpg",
+        imgSrc:"./assets/bookImg/book5.jpg",
       },
    
   ];
@@ -62,14 +62,14 @@ for(let i = 0; i<cart.length; i++){
 }
 
 function cartNumbers(){
-    let productNumbers = localStorage.getItem('cartNumbers');
+    let productNumbers = sessionStorage.getItem('cartNumbers');
     productNumbers = parseInt(productNumbers);
     if(productNumbers){
-        localStorage.setItem('cartNumbers', productNumbers+1);
+        sessionStorage.setItem('cartNumbers', productNumbers+1);
         document.querySelector('.cartnumber').textContent=productNumbers+1;    
     }
     else{
-    localStorage.setItem('cartNumbers', 1);
+    sessionStorage.setItem('cartNumbers', 1);
     console.log(document.querySelector('.cartnumber'));
     document.querySelector('.cartnumber').textContent=1;
     }
@@ -78,30 +78,120 @@ function cartNumbers(){
 
 function totalCost(product){
 
-    if( localStorage.getItem('totalCost') === null){
-        localStorage.setItem('totalCost', "0");
+    if( sessionStorage.getItem('totalCost') === null){
+        sessionStorage.setItem('totalCost', "0");
     }
     
-   let cartCost = localStorage.getItem('totalCost');
+   let cartCost = sessionStorage.getItem('totalCost');
     if(cart != null){
         
         
         cartCost = parseInt(cartCost);
-        localStorage.setItem('totalCost',cartCost+product.productPrice);
+        sessionStorage.setItem('totalCost',cartCost+product.productPrice);
     }
     else{
-        localStorage.setItem('totalCost',product.productPrice); 
+        sessionStorage.setItem('totalCost',product.productPrice); 
 
     }
 
 }
-// pass product co  cart page
-function cartHandler(){
-    document.querySelector(".items").textContent = localStorage.getItem("cartNumbers");
-    document.querySelector("#totalCost").textContent = localStorage.getItem("totalCost");
-    document.querySelector("#totalCost").textContent = localStorage.getItem("cartNames");
+
+//The Entire   Arrays and store to other side
+var cartArray = [];
+var itemArray ;
+var productCart = [];
+
+function addtoCart(id){
+
+    var quanitityIncreased = false;
+    //Find if cart already has items.
+    //If yes get items from sessionStorage and save them into array
+    if(sessionStorage.getItem("items") !== null){
+        cartArray = JSON.parse(sessionStorage.getItem("items"));
+
+    }
+    //find the same product
+    for(let i = 0; i < cartArray.length; i++){
+           if(cartArray[i].id === id){
+               cartArray[i].quanitity += 1;
+               quanitityIncreased = true;
+               break;
+           }
+           
+    }
     
+    if(!quanitityIncreased){
+        //Find product with the same id
+        for(let i = 0; i < products.length; i++){
+            //Add product to cart array
+            if(products[i].id===id){
+                itemArray = 
+                    {
+                        id: products[i].id,
+                        productName: products[i].productName,
+                        author:products[i].author,
+                        isbn:products[i].isbn,
+                        imgSrc: products[i].imgSrc,
+                        productPrice:products[i].productPrice,
+                        quanitity: 1 
+                    };
+                
+                cartArray.push(itemArray);
+                Alert="Item added to cart";
+            }
+        }
+        
+    }
+
+    //Save all cart product to sessionStorage
+    sessionStorage.setItem("items", JSON.stringify(cartArray));
+    Alert="Product added to cart";
+   
+    /*for (let i = 0; i < products.length; i++) {
+        if(cartArray == null ){
+            alert('Cart is Empty')
+        } else {
+            products.push(cartArray);
+        }
+        sessionStorage.setItem("item", JSON.stringify(cartArray[0]));
+      
+      
+    };*/
+ 
 }
+function pullinformation(){
+    cartArray = JSON.parse(sessionStorage.getItem("items"));
+    var cartItems = "";
+
+     for(let i = 0; i < cartArray.length; i++){
+        
+        cartItems +=` 
+        <div class="Cart-Items">
+            <div class="image-box">
+                <img src="${cartArray[i].imgSrc}"   style="height:120px"/>
+            </div>
+            <div class="about">
+                <h1 class="title">${cartArray[i].productName}</h1>
+                <h3 class="subtitle">${cartArray[i].isbn}</h3>
+            </div>
+            <div class="counter">
+                <div class="btn">+</div>
+                <div class="count">${cartArray[i].quanitity}</div>
+                <div class="btn">-</div>
+            </div>
+            <div class="prices">
+                <div class="amount">R ${cartArray[i].productPrice}</div>
+                <div class="save"><u>Save for later</u></div>
+                <div class="remove"><u>Remove</u></div>
+            </div>
+        </div>`;
+        
+    }
+    document.getElementById("Cart").innerHTML = cartItems;
+}
+
+// pass product co  cart page
+
 
 
 
